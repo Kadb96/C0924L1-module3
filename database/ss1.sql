9370d6a073eaa74da76a1a140d780d09f4f72aca
Kadb96 <ptvu.2838@gmail.com>
2025-02-07 20:51:27 +0700

    nop bai tap ss1

---
+++ database/ss1.sql
@@ -0,0 +1,11 @@
+create database student_management;
+create table teacher (
+	id varchar(100),
+	name varchar(100),
+	age tinyint,
+	country varchar(100)
+	);
+create table class (
+	id varchar(100),
+    name varchar(100)
+    );
