23a840020647fc90d27b1aba2018ccfa2eb2db0e
Kadb96 <ptvu.2838@gmail.com>
2025-02-10 20:28:19 +0700

    sua bai tap ss1

--- database/ss1/ss1.sql
+++ database/ss1/ss1.sql
@@ -1,11 +1,11 @@
 create database student_management;
 create table teacher (
-	id varchar(100),
+	id varchar(100) primary key,
 	name varchar(100),
 	age tinyint,
 	country varchar(100)
 	);
 create table class (
-	id varchar(100),
+	id varchar(100) primary key,
     name varchar(100)
     );
