9d528e344ef4ee8cd429f28be20e3ec2e245363c
Kadb96 <ptvu.2838@gmail.com>
2025-04-12 01:24:15 +0700

    task 1

--- database/casestudy/cau21_cau28.sql
+++ database/casestudy/cau21_cau28.sql
@@ -1,3 +1,6 @@
+-- Tạo khung nhìn có tên là v_nhan_vien để lấy được thông tin của 
+-- tất cả các nhân viên có địa chỉ là “Hải Châu” và đã từng lập hợp đồng cho một hoặc nhiều khách hàng bất kì với ngày lập hợp đồng là “12/12/2019”.
+
 CREATE VIEW v_nhan_vien AS
 SELECT nv.*
 FROM 
